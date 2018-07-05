1e0ddb1d404ef0e149e5966cf0c290a00b93f183
SarantopoulosKon <33217757+SarantopoulosKon@users.noreply.github.com>
2018-07-05 15:32:10 +0300

    Update youtube.js

--- services/youtube.js
+++ services/youtube.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const request = require('request-promise-native');
-const { key: apiKey } = require('../api_key.json');
+const { youtubeApiKey: apiKey } = require('../credentials.json');
 
 module.exports = function search(keyword) {
     const url = `https://content.googleapis.com/youtube/v3/search?q=${keyword}&maxResults=1&part=snippet&key=${apiKey}`;
